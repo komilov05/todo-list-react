cdada0c33028f26b337020afd1f836c42daf58ef
komilov05 <kkomilov733@gmail.com>
2023-01-31 15:20:28 +0500

    sucsessful ready

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,15 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext ,useState } from 'react'
 import { TodoContext } from '../context/TodoContext'
 
 const Item = () => {
     const { todos,deleteTodo } = useContext(TodoContext)
+    const [checked, setCheked] = useState(false)
+    console.log(checked)
     return (
         todos.map(todo => {
             return(
                 <div className='d-flex'>
-                    <li key={todo.id}>{todo.title}</li>
+                    <li className={"" + (checked ? "selected" : "") } key={todo.id}>{todo.title}</li>
                     <div className='d-flex'> 
-                        <input onChange={() => handleChange(todo.id)} type="checkbox"  />
+                        <input onChange={(e)=> setCheked(e.target.checked)} type="checkbox"  />
                         <button className='border' onClick={() => deleteTodo(todo.id)}>delete</button>
                     </div>
                 </div>
